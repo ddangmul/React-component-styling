266cd124eb60c7be4f058d1df6a4590d683c000d
ddangmul <j.ddangmul@gmail.com>
2024-11-01 21:36:44 +0900

    테일윈드 CSS 기존 스타일 불러오기 & 유틸리티클래스 커스터마이징

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // extend - fontFamily : 폰트 목록을 확장하겠다는 의미
+        title: ['"Pacifico"', "cursive"], // 구글폰트는 홑따옴표 안 쌍따옴표로 작성해야 함
+      },
+    },
   },
   plugins: [],
 };
